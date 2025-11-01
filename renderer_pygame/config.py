0063aa46878358a015ee4ecb7dceed7846056dd3
bernackimark <bernackimark@gmail.com>
2025-10-31 23:26:07 -0400

    initial commit

---
+++ renderer_pygame/config.py
@@ -0,0 +1,4 @@
+COLORS_W_COLORLESS = ('R', 'G', 'U', 'B', 'W', 'C')
+COLOR_NAMES = ('brown3', 'darkgreen', 'royalblue1', 'gray23', 'lemonchiffon', 'burlywood4')
+
+COLOR_DICT = {letter: name for letter, name in zip(COLORS_W_COLORLESS, COLOR_NAMES)}
