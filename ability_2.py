a5324ffaf44067a43e315d6eb67bf265ee783816
bernackimark <bernackimark@gmail.com>
2025-11-03 22:46:47 -0500

    initial commit

---
+++ ability_2.py
@@ -0,0 +1,8 @@
+def get_all_creatures(gs: "GameState") -> list["GameCard"]:
+    return [c for b in gs.boards for c in b.cards if c.props.is_creature]
+
+def send_all_creatures_to_graveyard(gs: "GameState") -> None:
+    for c in get_all_creatures(gs):
+        gs.send_to_graveyard(c)
+        gs.remove_from_board(c)
+
