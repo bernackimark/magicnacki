62598d48c4501698e33c88e9cfdf82260d214e9b
bernackimark <bernackimark@gmail.com>
2025-11-22 16:29:42 -0500

    initial commit

---
+++ models/modifiers.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class BasePT:
+    power: int
+    toughness: int
+
+
+@dataclass
+class PTModifier:
+    card: "GameCard"
+    power_delta: int = 0
+    toughness_delta: int = 0
+
+    def __repr__(self):
+        return f'{self.card.props.name}({self.power_delta}/{self.toughness_delta})'
+
+
+@dataclass
+class PTTemp:
+    power_delta: int
+    toughness_delta: int
+    expires_end_of_turn: bool = True
+
+
+@dataclass
+class KWAModifier:
+    card: "GameCard"
+    add_or_remove: str
+    kwa: str
+
+    def __repr__(self):
+        return f"{'gains' if self.add_or_remove == 'add' else 'loses'} {self.kwa}"
+
+
+@dataclass
+class KWATemp:
+    add_or_remove: str
+    kwa: str
+    expires_end_of_turn: bool = True
+
+    def __repr__(self):
+        return f"{'gains' if self.add_or_remove == 'add' else 'loses'} {self.kwa} until end of turn"
