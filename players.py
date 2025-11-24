9e2395f77d9872336fbf509e3540455981cc901b
bernackimark <bernackimark@gmail.com>
2025-11-23 20:49:09 -0500

    print info about any card

--- players.py
+++ players.py
@@ -25,5 +25,17 @@ class ConsolePlayer(Player):
         for i, avail_action in enumerate(avail_actions):
             print(f"{i}: {avail_action}")
         with suppress(KeyboardInterrupt):
-            sel_action: int = int(input("Please select an action "))
+            while True:
+                sel_action: str = input("Please select an action (or type a card slug to get info about it) ")
+                if sel_action.isnumeric():
+                    sel_action = int(sel_action)
+                    break
+                else:
+                    card_info = next((c for d in gs.decks_all_cards for c in d.cards if c.props.slug == sel_action), None)
+                    if not card_info:
+                        print('Card not found')
+                        continue
+                    props = card_info.props
+                    print(props.name, props.casting_cost, props.card_types, props.card_sub_types,
+                          props.keyword_abilities, props.oracle_rules_text)
             return avail_actions[sel_action]
