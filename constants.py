4630f3f94206c37cd5cee7fc3954dc2329188097
bernackimark <bernackimark@gmail.com>
2026-01-01 16:06:31 -0500

    + OLD_SCHOOL_SETS

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ BASIC_LANDS = ('forest', 'island', 'mountain', 'plains', 'swamp')
 COLOR_LETTERS = ('G', 'U', 'R', 'W', 'B')
 BASIC_LAND_MANA_PRODUCED = {slug: color_letter for slug, color_letter in zip(BASIC_LANDS, COLOR_LETTERS)}
 COLOR_LETTERS_W_COLORLESS = ('G', 'U', 'R', 'W', 'B', 'C')
+OLD_SCHOOL_SETS = ('1E', '2E', '2U', '3E', 'AN', 'AQ', 'DK', 'LE')
