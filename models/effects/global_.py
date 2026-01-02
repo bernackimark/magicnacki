5d520e7011a490c311263fdea62cd2713268ff50
bernackimark <bernackimark@gmail.com>
2026-01-01 22:08:23 -0500

    + angelic_voices, bad_moon

--- models/effects/global_.py
+++ models/effects/global_.py
@@ -22,6 +22,30 @@ class GlobalEffect:
     def resolve(self):
         ...
 
+class AngelicVoicesEffect(GlobalEffect):
+    def __init__(self, owner_id: int):
+        self.owner_id = owner_id
+
+    def applies_to(self, card, gs: "GameState") -> bool:
+        # All of your creatures, so long as you don't have any creatures of another color (artifacts are OK)
+        for my_creature in CardFilter(gs).creatures().on_player_board(self.owner_id).result():
+            if 'W' not in my_creature.props.colors or 'C' not in my_creature.props.colors:
+                return False
+        return True
+
+    def pt_offset(self, card=None, power=None, toughness=None):
+        return 1, 1
+
+class BadMoonEffect(GlobalEffect):
+    def __init__(self, owner_id: Optional[int] = None):
+        self.owner_id = owner_id  # Optional, can affect all players
+
+    def applies_to(self, card, gs: "GameState") -> bool:
+        # All untapped creatures on any board (or specific player if owner_id set)
+        return card in CardFilter(gs).in_play().creatures().black().result()
+
+    def pt_offset(self, card=None, power=None, toughness=None):
+        return 1, 1
 
 class CastleEffect(GlobalEffect):
     def __init__(self, owner_id: int):
