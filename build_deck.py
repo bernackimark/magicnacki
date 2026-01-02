258b76bfc7205457a77616bbdf7b1c8641012fd9
bernackimark <bernackimark@gmail.com>
2026-01-01 21:00:02 -0500

    + old school banned & restricted lists

--- build_deck.py
+++ build_deck.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
+from typing import Iterable
 
 from card import Card, CardUniverse
-from constants import BASIC_LANDS
+from constants import BASIC_LANDS, OLD_SCHOOL_BANNED_SLUGS, OLD_SCHOOL_RESTRICTED_SLUGS
 from models.game_card import GameCard
 
 
@@ -17,7 +18,9 @@ class DeckBuilder:
     cards: list[GameCard] = field(default_factory=list)
     min_deck_size: int = 40
     max_deck_size: int = 99
-    max_non_basic_land_instances: int = 4
+    play_set_cnt: int = 4
+    banned_slugs: Iterable[str] = OLD_SCHOOL_BANNED_SLUGS
+    restricted_slugs: Iterable[str] = OLD_SCHOOL_RESTRICTED_SLUGS
 
     @property
     def _next_card_id(self) -> int:
@@ -33,16 +36,19 @@ class DeckBuilder:
         return sum([1 for c in self.cards if c.props.slug == slug]) if self.cards else 0
 
     def add_card(self, c: Card) -> None:
-        if c.slug not in BASIC_LANDS and self.get_slug_cnt(c.slug) >= self.max_non_basic_land_instances:
-            raise ValueError(f"You can only have {self.max_non_basic_land_instances} instances of {c.name}")
+        """Card cannot be on the banned list; only one allowed from restricted; can only have cnt up to max play set"""
+        if c.slug in self.banned_slugs:
+            raise ValueError(f"{c.name} is on the Banned List and cannot be added to your deck")
+        if c.slug in self.restricted_slugs and self.get_slug_cnt(c.slug) == 1:
+            raise ValueError(f"{c.name} is on the Restricted List and you already have one in your deck")
+        if c.slug not in BASIC_LANDS and self.get_slug_cnt(c.slug) >= self.play_set_cnt:
+            raise ValueError(f"You can only have the max play set ({self.play_set_cnt}) for {c.name}")
         game_card = GameCard(self.card_universe[c.slug], self._next_card_id, self.player_idx)
         self.cards.append(game_card)
 
     def add_card_by_slug(self, slug: str):
-        if slug not in BASIC_LANDS and self.get_slug_cnt(slug) >= self.max_non_basic_land_instances:
-            raise ValueError(f"You can only have {self.max_non_basic_land_instances} instances of {slug}")
-        game_card = GameCard(self.card_universe[slug], self._next_card_id, self.player_idx)
-        self.cards.append(game_card)
+        card = next(c for c in self.card_universe.cards if c.slug == slug)
+        self.add_card(card)
 
     def remove_card(self, c: GameCard) -> None:
         if c not in self.cards:
--- constants.py
+++ constants.py
@@ -3,3 +3,10 @@ COLOR_LETTERS = ('G', 'U', 'R', 'W', 'B')
 BASIC_LAND_MANA_PRODUCED = {slug: color_letter for slug, color_letter in zip(BASIC_LANDS, COLOR_LETTERS)}
 COLOR_LETTERS_W_COLORLESS = ('G', 'U', 'R', 'W', 'B', 'C')
 OLD_SCHOOL_SETS = ('1E', '2E', '2U', '3E', 'AN', 'AQ', 'DK', 'LE')
+OLD_SCHOOL_RESTRICTED_SLUGS = ('ancestral-recall', 'black-lotus', 'braingeyser', 'brainstorm', 'candelabra-of-tawnos',
+                               'chaos-orb', 'copy-artifact', 'demonic-tutor', 'ivory-tower', 'library-of-alexandria',
+                               'mana-drain', 'mirror-universe', 'mishras-workshop', 'mox-sapphire', 'mox-jet',
+                               'mox-ruby', 'mox-pearl', 'mox-emerald', 'regrowth', 'sol-ring', 'strip-mine',
+                               'time-walk', 'timetwister', 'wheel-of-fortune')
+OLD_SCHOOL_BANNED_SLUGS = ('bronze-tablet', 'contract-from-below', 'darkpact', 'demonic-attorney',
+                           'divine-intervention', 'jeweled-bird', 'rebirth', 'shahrazad', 'tempest-efreet')
