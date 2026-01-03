b0a91b9326f2e7b85342760cd67777d9057341c3
bernackimark <bernackimark@gmail.com>
2026-01-03 17:49:53 -0500

    + ivory-tower

--- models/effects/upkeep.py
+++ models/effects/upkeep.py
@@ -1,4 +1,8 @@
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from ..game_card import GameCard
+    from game_state import GameState
 
 from models.effects.base import Effect
 from card_filter import CardFilter
@@ -29,6 +33,17 @@ def feedback_on_upkeep():
         
         def resolve(self, gs, source: "GameCard", target: Optional["GameCard"] = None):
             gs.decrement_life(gs.player_turn_idx, 1, source)
+    return E()
+
+def ivory_tower_on_upkeep():
+    class E(Effect):
+        event = 'upkeep'
+
+        def resolve(self, gs: GameState, source: GameCard, target: Optional[GameCard] = None):
+            # At the beginning of your upkeep, you gain X life, where X is the number of cards in your hand minus 4
+            p_id = source.orig_owner_id
+            if (hand_size := len(gs.hands[p_id].cards)) > 4:
+                gs.increment_life(p_id, hand_size - 4)
     return E()
 
 def karma_on_upkeep():
