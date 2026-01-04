1b5c420e2d529d6fed45869dd5b889c1e1853d6e
bernackimark <bernackimark@gmail.com>
2026-01-03 20:13:52 -0500

    fixed type hinting

--- models/effects/upkeep.py
+++ models/effects/upkeep.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
@@ -13,7 +14,7 @@ def copper_tablet_on_upkeep():
     class E(Effect):
         event = 'upkeep'
 
-        def resolve(self, gs, source: "GameCard", target: Optional["GameCard"] = None):
+        def resolve(self, gs: GameState, source: GameCard, target: Optional[GameCard] = None):
             gs.decrement_life(gs.player_turn_idx, 1, source)
     return E()
 
@@ -22,7 +23,7 @@ def cursed_land_on_upkeep():
     class E(Effect):
         event = 'upkeep'
 
-        def resolve(self, gs, source: "GameCard", target: Optional["GameCard"] = None):
+        def resolve(self, gs: GameState, source: GameCard, target: Optional[GameCard] = None):
             gs.decrement_life(target.orig_owner_id, 1, source)
 
     return E()
@@ -31,7 +32,7 @@ def feedback_on_upkeep():
     class E(Effect):
         event = 'upkeep'
         
-        def resolve(self, gs, source: "GameCard", target: Optional["GameCard"] = None):
+        def resolve(self, gs: GameState, source: GameCard, target: Optional[GameCard] = None):
             gs.decrement_life(gs.player_turn_idx, 1, source)
     return E()
 
@@ -50,7 +51,7 @@ def karma_on_upkeep():
     class E(Effect):
         event = 'upkeep'
         
-        def resolve(self, gs, source: "GameCard", target: Optional["GameCard"] = None):
+        def resolve(self, gs: GameState, source: GameCard, target: Optional[GameCard] = None):
             opponent = flip(gs.player_turn_idx)
             swamp_list = CardFilter(gs).on_player_board(opponent).by_slug('swamp').result()
             swamp_cnt = len(swamp_list)
@@ -62,6 +63,6 @@ def serendib_efreet_on_upkeep():
     class E(Effect):
         event = 'upkeep'
         
-        def resolve(self, gs, source: "GameCard", target: Optional["GameCard"] = None):
+        def resolve(self, gs: GameState, source: GameCard, target: Optional[GameCard] = None):
             gs.decrement_life(gs.player_turn_idx, 1, source)
     return E()
