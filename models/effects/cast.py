270a3db850415b005a07e282e3753619fcfd780c
bernackimark <bernackimark@gmail.com>
2026-01-04 18:52:48 -0500

    drain_power_on_cast refactored to support new ManaPool approach

--- models/effects/cast.py
+++ models/effects/cast.py
@@ -175,7 +175,7 @@ def dark_ritual_on_cast():
         event = 'cast'
 
         def resolve(self, gs: GameState, source: GameCard, target: Optional[int] = None):
-            gs.mana_pools[source.orig_owner_id].add('B', 3)
+            gs.mana_pools[source.orig_owner_id].add_floating('B', 3)
     return E()
 
 def demonic_torment_on_cast():
@@ -243,9 +243,10 @@ def drain_power_on_cast():
             """target = player_id whose available mana will be targeted & given to the other player"""
             if target is None:
                 return
-            land_giver_mana = gs.mana_pools[target].mana
-            land_receiver_mana = gs.mana_pools[flip(target)].mana
-            land_receiver_mana = {k: land_giver_mana[k] + land_receiver_mana[k] for k in land_giver_mana}
+            land_giver_mana = gs.mana_pools[target].available_mana.copy()
+            land_taker_id = flip(target)
+            for color, amt in land_giver_mana.items():
+                gs.mana_pools[land_taker_id].add_floating(color, amt)
             print(f"{source} steals all of Player #{target}'s unused mana.")
     return E()
 
@@ -280,7 +281,7 @@ def energy_tap_on_cast():
                 return
             target.tap(gs)
             mana_value = source.props.casting_weight
-            gs.mana_pools[source.orig_owner_id].add('C', mana_value)
+            gs.mana_pools[source.orig_owner_id].add_floating('C', mana_value)
             print(f"{source} taps to add {mana_value} colorless to your mana pool.")
     return E()
 
