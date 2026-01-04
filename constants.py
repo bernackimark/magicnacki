bfc3082f274e4acbae9c7d40a12b7e708aadcc89
bernackimark <bernackimark@gmail.com>
2026-01-04 18:45:05 -0500

    + some color constants

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
 BASIC_LANDS = ('forest', 'island', 'mountain', 'plains', 'swamp')
 COLOR_LETTERS = ('G', 'U', 'R', 'W', 'B')
+COLOR_LETTERS_W_COLORLESS = ('G', 'U', 'R', 'W', 'B', 'C')  # 'C' must be last in the sequence for mana handling
+COLOR_LETTER_SLUG = {letter: slug for letter, slug in zip(COLOR_LETTERS, BASIC_LANDS)}
 BASIC_LAND_MANA_PRODUCED = {slug: color_letter for slug, color_letter in zip(BASIC_LANDS, COLOR_LETTERS)}
-COLOR_LETTERS_W_COLORLESS = ('G', 'U', 'R', 'W', 'B', 'C')
+
 OLD_SCHOOL_SETS = ('1E', '2E', '2U', '3E', 'AN', 'AQ', 'DK', 'LE')
 OLD_SCHOOL_RESTRICTED_SLUGS = ('ancestral-recall', 'black-lotus', 'braingeyser', 'brainstorm', 'candelabra-of-tawnos',
                                'chaos-orb', 'copy-artifact', 'demonic-tutor', 'ivory-tower', 'library-of-alexandria',
