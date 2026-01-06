2a4eace72c6498b5432f66728341f2a627dccfa1
bernackimark <bernackimark@gmail.com>
2026-01-06 16:08:52 -0500

    + marble-priest

--- models/effects/damage_prevention.py
+++ models/effects/damage_prevention.py
@@ -5,8 +5,6 @@ if TYPE_CHECKING:
     from game_state import GameState
 
 from ..damage import DamageEvent
-
-
 from models.effects.base import Effect
 
 def argothian_pixies_damage_prevention():
@@ -14,10 +12,8 @@ def argothian_pixies_damage_prevention():
     class E(Effect):
         def on_damage(self, gs: GameState, event: DamageEvent):
             # don't have access to GameCard due to circular import; using hasattr() to see if source is a card
-            if (not hasattr(event.source.props, 'slug') or not hasattr(event.target.props, 'slug') or
-                    event.target.props.slug != 'argothian-pixies'):
+            if not hasattr(event.target, 'props') or event.target.props.slug != 'argothian-pixies':
                 return
-
             if 'Artifact' in event.source.props.card_types and 'Creature' in event.source.props.card_types:
                 event.prevented += event.remaining
     return E()
@@ -26,11 +22,28 @@ def argothian_treefolk_damage_prevention():
     """Prevent all damage that would be dealt to this creature by artifact sources"""
     class E(Effect):
         def on_damage(self, gs: GameState, event: DamageEvent):
-            if (not hasattr(event.source.props, 'slug') or not hasattr(event.target.props, 'slug') or
-                    event.target.props.slug != 'argothian-treefolk'):
+            if not hasattr(event.target, 'props') or event.target.props.slug != 'argothian-treefolk':
                 return
-
-            # Source must be an artifact creature
             if 'Artifact' in event.source.props.card_types:
+                event.prevented += event.remaining
+    return E()
+
+def artifact_ward_damage_prevention():
+    """Prevent all damage that would be dealt to enchanted creature by artifact sources"""
+    class E(Effect):
+        def on_damage(self, gs: GameState, event: DamageEvent):
+            if not hasattr(event.target, 'props') or not event.target.modifiers.is_enchanted_by('artifact-ward'):
+                return
+            if 'Artifact' in event.source.props.card_types:
+                event.prevented += event.remaining
+    return E()
+
+def marble_priest_damage_prevention():
+    """Prevent all combat damage that would be dealt to this creature by Walls"""
+    class E(Effect):
+        def on_damage(self, gs: GameState, event: DamageEvent):
+            if not hasattr(event.target, 'props') or event.target.props.slug != 'marble_priest':
+                return
+            if event.is_combat and 'Wall' in event.source.props.card_sub_types:
                 event.prevented += event.remaining
     return E()
