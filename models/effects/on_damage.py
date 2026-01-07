35ede1f8fddb3ff1cea9079dcea5c7e5fe3533c7
bernackimark <bernackimark@gmail.com>
2026-01-07 18:18:50 -0500

    initial commit

---
+++ models/effects/on_damage.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from base import Effect
+from ..damage import DamageEvent
+
+if TYPE_CHECKING:
+    from ..game_card import GameCard
+    from game_state import GameState
+
+def martyrs_of_korlis_on_damage():
+    """As long as this creature is untapped,
+    all damage that would be dealt to you by artifacts is dealt to this creature instead"""
+    class E(Effect):
+        event = 'on_damage'
+
+        def on_damage(self, gs: GameState, event: DamageEvent):
+            if not isinstance(event.target, int):
+                return
+            if not (martyrs := gs.card_filter.on_player_board(event.target).by_slug('martyrs-of-korlis').untapped().result()):
+                return
+            if "Artifact" not in event.source.props.card_types:
+                return
+
+            # use the first found untapped martys-of-korlis owned by the damaged player
+            event.target = martyrs[0]
+    return E()
