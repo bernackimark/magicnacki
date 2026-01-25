d0ecefe4672b9b6b8d9e011743d3c5befe14c4a9
bernackimark <bernackimark@gmail.com>
2026-01-24 19:47:15 -0500

    + add_poison_counter funcs

--- models/effects/on_damage.py
+++ models/effects/on_damage.py
@@ -1,20 +1,52 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from utils import flip
 from .base import Effect
-from ..counter_tokens import PLUS_ONE, VITALITY
+from ..counter_tokens import PLUS_ONE, VITALITY, POISON
 from ..damage import DamageEvent
 
 if TYPE_CHECKING:
     from ..game_card import GameCard
     from game_state import GameState
 
+def add_poison_counter_on_damage():
+    """Whenever this creature deals damage to a player, that player gets a poison counter"""
+
+    class E(Effect):
+        event = 'on_damage'
+
+        def resolve(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
+            opp = flip(this_card.orig_owner_id)
+            if event.source == this_card and event.target == opp:
+                gs.add_poison_counter(opp)
+                print(f"{event.source.props.name} adds a poison counter to Player #{opp}. "
+                      f"Poison Totals: {gs.poison_counters}")
+
+    return E()
+
+
+def add_two_poison_counters_on_damage():
+    """Whenever this creature deals damage to a player, that player gets two poison counters"""
+
+    class E(Effect):
+        event = 'on_damage'
+
+        def resolve(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
+            opp = flip(this_card.orig_owner_id)
+            if event.source == this_card and event.target == flip(opp):
+                print(f"{event.source.props.name} adds two poison counters to Player #{opp}. "
+                      f"Poison Totals: {gs.poison_counters}")
+                gs.add_poison_counter(opp, 2)
+
+    return E()
+
 def fungusaur_on_damage():
     """Whenever this creature is dealt damage, put a +1/+1 counter on it"""
     class E(Effect):
         event = 'on_damage'
 
-        def on_damage(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
+        def resolve(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
             if event.target == this_card:
                 this_card.counters.add_counter(PLUS_ONE)
     return E()
@@ -24,7 +56,7 @@ def living_artifact_on_damage():
     class E(Effect):
         event = 'on_damage'
 
-        def on_damage(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
+        def resolve(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
             if event.target == this_card.orig_owner_id:
                 this_card.counters.add_counter(VITALITY)
     return E()
@@ -35,7 +67,7 @@ def martyrs_of_korlis_on_damage():
     class E(Effect):
         event = 'on_damage'
 
-        def on_damage(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
+        def resolve(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
             if this_card.is_tapped:
                 return
             if event.target != this_card.orig_owner_id:
@@ -50,7 +82,7 @@ def spirit_link_on_damage():
     class E(Effect):
         event = 'on_damage'
 
-        def on_damage(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
+        def resolve(self, gs: GameState, event: DamageEvent, this_card: GameCard = None):
             if event.source == this_card.attached_to:
                 gs.increment_life(this_card.attached_to.orig_owner_id, event.remaining)
     return E()
