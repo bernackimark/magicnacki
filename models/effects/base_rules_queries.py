68d200dd0524b92a9d06ae8d6895107bbe1b9afe
bernackimark <bernackimark@gmail.com>
2026-01-31 15:54:41 -0500

    initial commit

---
+++ models/effects/base_rules_queries.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game_state import GameState
+    from models.game_card import GameCard
+
+from constants import BASIC_LANDS
+from models.effects.base import Effect
+from utils import flip
+
+
+class CanBlockBaseRule(Effect):
+    event = 'can_block'
+
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: card = blocker, mandatory kwarg: attacker"""
+        if event != "can_block":
+            return None
+        attacker: GameCard = kwargs.get("attacker")
+        if not attacker or not card:
+            return None
+
+        # Global land walk rule
+        defender_idx = card.orig_owner_id
+        for walk, basic_land in zip([land.capitalize() + 'walk' for land in BASIC_LANDS], BASIC_LANDS):
+            if walk in attacker.keyword_abilities and gs.card_filter.on_player_board(defender_idx).by_slug(basic_land).result():
+                return False
+
+        # Global Flying/Reach rule
+        if ('Flying' in attacker.keyword_abilities and
+                not any(kwa for kwa in card.keyword_abilities if kwa in ('Flying', 'Reach'))):
+            return False
+
+        return None  # no opinion if can_block
+
+
+class CanAttackBaseRule(Effect):
+    event = 'can_attack'
+
+    def on_query(self, gs: GameState, event: str, **kwargs):
+        """kwargs = includes 'card' when checking if a card can attack"""
+        if event != 'can_attack' or not kwargs.get('card'):
+            return None
+
+        card: GameCard = kwargs.get('card')
+
+        if not card.props.is_creature or card.has_summoning_sickness or card.is_tapped or 'Attack' not in card.abilities:
+            return False
+
+        if 'Islandhome' in card.abilities:
+            if not gs.card_filter.on_player_board(flip(card.orig_owner_id)).by_slug('island').result():
+                return False
+
+        return None  # no opinion on whether the card can attack
---
+++ models/effects/queries.py
@@ -0,0 +1,151 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from models.modifiers import PTModifier
+
+if TYPE_CHECKING:
+    from game_state import GameState
+    from models.game_card import GameCard
+
+from models.effects.base import Effect
+
+
+# --- CARD-SPECIFIC ---
+class AmrouKithkin(Effect):
+    event = 'query'
+
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: can_block, card = 'amrou-kithkin', mandatory kwargs: blocker"""
+        blocker: GameCard = kwargs.get("blocker")
+        if event != "can_be_blocked" or card.props.slug != 'amrou-kithkin' or not blocker:
+            return None
+        if blocker.power > 3:
+            return False
+
+class AngelicVoices(Effect):
+    """Creatures you control get +1/+1 as long as you control no nonartifact, nonwhite creatures."""
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        if event != 'pt_mod':
+            return None
+        for my_creature in gs.card_filter.creatures().on_player_board(card.orig_owner_id).result():
+            if 'W' not in my_creature.props.colors or 'C' not in my_creature.props.colors:
+                return False
+        return PTModifier(card, 1, 1)
+
+class ArtifactWardCanBeBlocked(Effect):
+    """This creature can't be blocked by artifact creatures"""
+    event = 'query'
+
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: can_block, card = the enchanted card, mandatory kwargs: blocker"""
+        blocker: GameCard = kwargs.get("blocker")
+        if event != "can_be_blocked" or not blocker or not card.modifiers.is_enchanted_by('artifact-ward'):
+            return None
+        if 'Artifact' in blocker.props.card_types:
+            return False
+
+class ArgothianPixiesCanBeBlocked(Effect):
+    event = 'query'
+
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: can_block, card = 'argothian-pixies', mandatory kwargs: blocker"""
+        blocker: GameCard = kwargs.get("blocker")
+        if event != "can_be_blocked" or card.props.slug != 'argothian-pixies' or not blocker:
+            return None
+        if 'Artifact' in blocker.props.card_types:
+            return False
+
+class BadMoon(Effect):
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        if event != 'pt_mod':
+            return None
+        if card not in gs.card_filter.in_play().black().creatures().result():
+            return None
+        return PTModifier(card, 1, 1)
+
+class BogRats(Effect):
+    event = 'query'
+
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: can_block, card = 'bog-rats', mandatory kwargs: blocker"""
+        blocker: GameCard = kwargs.get("blocker")
+        if event != "can_be_blocked" or card.props.slug != 'bog-rats' or not blocker:
+            return None
+        if 'Wall' in blocker.props.card_sub_types:
+            return False
+
+class Castle(Effect):
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        if event != 'pt_mod':
+            return None
+        if card not in gs.card_filter.creatures().on_player_board(card.orig_owner_id).tapped(False).white().result():
+            return None
+        return PTModifier(card, 0, 2)
+
+class Crusade(Effect):
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        if event != 'pt_mod':
+            return None
+        if card not in gs.card_filter.in_play().white().creatures().result():
+            return None
+        return PTModifier(card, 1, 1)
+
+class ElderSpawnCanBeBlocked(Effect):
+    event = 'query'
+
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: can_block, card = 'elder-spawn', mandatory kwargs: blocker"""
+        blocker: GameCard = kwargs.get("blocker")
+        if event != "can_be_blocked" or card.props.slug != 'elder-spawn' or not blocker:
+            return None
+        if 'R' in blocker.props.colors:
+            return False
+
+class ElvenRidersCanBeBlocked(Effect):
+    event = 'query'
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: can_block, card = 'elven-riders', mandatory kwargs: blocker"""
+        blocker: GameCard = kwargs.get("blocker")
+        if event != "can_be_blocked" or card.props.slug != 'elven-riders' or not blocker:
+            return None
+        if 'Wall' not in blocker.props.card_sub_types or 'Flying' not in blocker.keyword_abilities:
+            return False
+
+class EvilEyeOfOrmsByGoreCanBeBlocked(Effect):
+    event = 'query'
+
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: can_block, card = 'EEOOBG', req'd kwargs: blocker. This creature can only be blocked by Walls"""
+        blocker: GameCard = kwargs.get("blocker")
+        if event != "can_be_blocked" or card.props.slug != 'evil-eye-of-orms-by-gore' or not blocker:
+            return None
+        if 'Wall' not in blocker.props.card_sub_types:
+            return False
+
+class KirdApePT(Effect):
+    event = 'query'
+
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        if event != 'pt_mod' or card.props.slug != 'kird-ape':
+            return None
+
+        if gs.card_filter.on_player_board(card.orig_owner_id).by_slug('forest').result():
+            return PTModifier(card, 1, 2)
+
+class Seeker(Effect):
+    """Enchanted creature can't be blocked except by artifact creatures and/or white creatures"""
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        """Query: can_block, card should be "seeker's" host, mandatory kwarg: blocker"""
+        blocker: GameCard = kwargs.get("blocker")
+        if event != "can_be_blocked" or card.attached_to.props.slug != 'seeker' or not blocker:
+            return None
+        if 'Artifact' not in blocker.props.card_types or 'U' not in blocker.props.colors:
+            return False
+
+class SunkenCity(Effect):
+    def on_query(self, gs: GameState, event: str, card: GameCard, **kwargs):
+        if event != 'pt_mod':
+            return None
+        if card not in gs.card_filter.in_play().blue().creatures().result():
+            return None
+        return PTModifier(card, 1, 1)
