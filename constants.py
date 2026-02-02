a648974085d87498ea3abd34bf653a4201f765f1
bernackimark <bernackimark@gmail.com>
2026-02-01 22:43:16 -0500

    + KEYWORD_ABILITIES

--- constants.py
+++ constants.py
@@ -18,3 +18,10 @@ OLD_SCHOOL_RESTRICTED_SLUGS = ('ancestral-recall', 'black-lotus', 'braingeyser',
 OLD_SCHOOL_BANNED_SLUGS = ('bronze-tablet', 'contract-from-below', 'darkpact', 'demonic-attorney',
                            'divine-intervention', 'jeweled-bird', 'rebirth', 'shahrazad', 'tempest-efreet')
 Target = Union["GameCard", list["GameCard"], int, tuple[int, int], None]
+
+KEYWORD_ABILITIES = ['Attack', 'Banding', 'Defender', 'First Strike', 'Flying', 'Forestwalk', 'Haste', 'Islandwalk',
+                     'Menace',
+                     'Mountainwalk', 'Protection From Black', 'Protection From Blue', 'Protection From Green',
+                     'Protection From Red', 'Protection From White', 'Rampage 1', 'Rampage 2', 'Rampage 3', 'Reach',
+                     'Swampwalk', 'Foresthome', 'Islandhome', 'Mountainhome', 'Plainswalk', 'Swamphome', 'Trample',
+                     'Vigilance']
