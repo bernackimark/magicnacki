74cfd99a73751b00bd2a2fc605766245afff4dff
bernackimark <bernackimark@gmail.com>
2026-02-17 19:08:33 -0500

    + initial commit

---
+++ tests/slugs.py
@@ -0,0 +1,13 @@
+import json
+
+from models.card_attributes.card_effect_specs import INVOCATIONS
+
+def all_slug_mappings_are_legitimate():
+    with open('/Users/Bernacki_Laptop/PycharmProjects/magicnacki/testing/card_statuses.json', 'r') as f:
+        all_cards: dict[str: str] = json.load(f)
+
+    for slug in INVOCATIONS:
+        assert slug in all_cards, f'{slug} not found'
+
+
+all_slug_mappings_are_legitimate()
