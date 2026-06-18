4ce5347cc883cd4352cab1c723939553d69df177
bernackimark <bernackimark@gmail.com>
2026-06-17 20:04:48 -0400

    + creature-bond

--- tests/test_cards_a_to_c.py
+++ tests/test_cards_a_to_c.py
@@ -11,7 +11,7 @@ class TestCombat(unittest.TestCase):
         self.engine.gs = self.engine.match_manager.create_game_state()
         self.gs = self.engine.gs
 
-    def test_blood_lust_1(self):
+    def test_blood_lust(self):
         """If target creature has toughness 5 or greater, it gets +4/-4 until end of turn.
         Otherwise, it gets +4/-X until end of turn, where X is its toughness minus 1."""
         large_creature = get_card(self.gs, 'bartel-runeaxe', 0)  # 6/5
@@ -25,6 +25,14 @@ class TestCombat(unittest.TestCase):
         self.assertEqual(large_creature.toughness, 1)
         self.assertEqual(small_creature.power, 5)
         self.assertEqual(small_creature.toughness, 1)
+
+    def test_creature_bond(self):
+        """When host dies, this Aura deals damage equal to that creature's toughness to the creature's controller."""
+        host = get_card(self.gs, 'merfolk-of-the-pearl-trident', 0)  # 1/1
+        creature_bond = get_card(self.gs, 'creature-bond', 1)
+        host.auras.append(creature_bond)
+        self.gs.pile_mgr.destroy(host)
+        self.assertEqual(self.gs.score_mgr.life[0], 19)
 
 
 if __name__ == '__main__':
