d4b4f2fecace933f8b5678c68f88bb84c224286b
bernackimark <bernackimark@gmail.com>
2026-06-24 19:00:49 -0400

    added tests

--- tests/test_available_actions_from_hand.py
+++ tests/test_available_actions_from_hand.py
@@ -1,5 +1,6 @@
 import unittest
 
+from models.actions.cast import BeginSpellCastAction
 from tests.setup_helpers import create_engine_and_universe, get_card, add_to_battlefield
 
 
@@ -18,6 +19,19 @@ class TestAvailableActionsFromHand(unittest.TestCase):
         aura = get_card(self.gs, 'holy-strength', 0)
         self.gs.pile_mgr.hands[0].cards.append(aura)
         self.assertIn('Cast Holy Strength', [a.__repr__() for a in self.gs.available_actions_from_hand()])
+
+    def test_can_cast_aura_to_multiple_valid_targets(self):
+        mana = get_card(self.gs, 'plains', 0)
+        creature = get_card(self.gs, 'savannah-lions', 0)
+        creature2 = get_card(self.gs, 'tundra-wolves', 0)
+        add_to_battlefield(mana, self.gs)
+        add_to_battlefield(creature, self.gs)
+        add_to_battlefield(creature2, self.gs)
+        aura = get_card(self.gs, 'holy-strength', 0)
+        self.gs.pile_mgr.hands[0].cards.append(aura)
+        BeginSpellCastAction(0, self.gs, aura, eff_spec=aura.triggered_abilities[0]).play()
+        target_cnt = len(self.gs.pending_choice.get_actions())
+        self.assertEqual(target_cnt, 2)
 
     def test_cannot_cast_auras_without_valid_target(self):
         mana = get_card(self.gs, 'plains', 0)
