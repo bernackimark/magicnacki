990de6fa8a6a050fa379b338318b989fe32bdf52
bernackimark <bernackimark@gmail.com>
2026-09-01 22:50:04 -0400

    initial commit

---
+++ models/effects/modifiers_generic.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game_state import GameState
+    from models.game_card.game_card import GameCard
+    from models.events_all import DamageProposedEvent
+
+from models.effects.base import Modifier
+
+class PreventDamage(Modifier):
+    def modify(self, gs: GameState, source: GameCard, event: DamageProposedEvent) -> None:
+        event.prevented += event.remaining
+        event.remaining = 0
