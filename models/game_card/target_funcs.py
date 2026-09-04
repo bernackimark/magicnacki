2451b96ae7216b3afdeafd05ace8de62540f25b7
bernackimark <bernackimark@gmail.com>
2026-09-04 16:36:05 -0400

    initial commit

---
+++ models/game_card/target_funcs.py
@@ -0,0 +1,26 @@
+from models.utils import flip
+
+class ET:
+    """Event -> Target.
+    Target funcs that return a lambda accepting GameState, source: GameState, event: Event;
+    returns a single target (int or GameCard)
+    Currently used to convey target info from the event to the resolver via On.t()"""
+    @staticmethod
+    def event_card_owner():
+        return lambda gs, source, event: event.card.owner_id
+
+    @staticmethod
+    def host_owner():
+        return lambda gs, source, event: source.host.owner_id
+
+    @staticmethod
+    def in_turn_p():
+        return lambda gs, source, event: gs.turn_mgr.player_turn_idx
+
+    @staticmethod
+    def opp():
+        return lambda gs, source, event: flip(source.owner_id)
+
+    @staticmethod
+    def s_owner():
+        return lambda gs, source, event: source.owner_id
